cceda7eb8087b49a02689540e863ada643d1e541
Ruan Melo <ruantmelo@gmail.com>
2024-01-25 14:58:21 -0300

    Adicionando eslint

--- App.js
+++ App.js
@@ -1,38 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, SafeAreaView, Pressable, ScrollView } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './screens/Login';
-import Registro from './screens/Registro';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  Pressable,
+  ScrollView,
+} from "react-native";
+
+import Login from "./screens/Login";
+import Registro from "./screens/Registro";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen name = "Login" component={Login} options={{headerShown: false}}/>
-          <Stack.Screen  name = "Registro" component={Registro} options ={{
-            title: 'Cadastro',
-            headerTitleAlign: 'center',
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Registro"
+          component={Registro}
+          options={{
+            title: "Cadastro",
+            headerTitleAlign: "center",
             headerTitleStyle: {
               fontSize: 30,
-              fontWeight: 'bold',
+              fontWeight: "bold",
             },
             headerShadowVisible: false,
-            }}
-            />  
-        </Stack.Navigator> 
-      </NavigationContainer>
-    
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     //alignItems: 'center',
     //justifyContent: 'center',
   },
